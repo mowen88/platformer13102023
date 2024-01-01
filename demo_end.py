e61f4c3bf98e46540b17509a9d988d8f01e8ade2
mowen88 <mattowen88@outlook.com>
2024-01-01 22:19:03 +0000

    01012024

---
+++ demo_end.py
@@ -0,0 +1,28 @@
+
+import pygame, math
+from state import State
+from settings import *
+
+class Intermission(State):
+	def __init__(self, game, scene):
+		State.__init__(self, game)
+
+		self.time_elapsed = COMMIT_SAVE_DATA['time_elapsed']
+
+	def update(self, dt):
+
+		if ACTIONS['enter']:
+			# timer reset and stop 
+			self.game.timer.stop_start()
+			self.game.timer.reset()
+			self.game.write_game_time()
+
+			self.exit_state()
+			self.prev_state.exit_state()
+			self.game.reset_keys()
+
+	def draw(self, screen):
+		screen.fill(BLACK)
+		self.game.render_text('Thanks for playing this demo !', WHITE, self.game.ui_font, (WIDTH * 0.5, HEIGHT * 0.4))
+		self.game.render_text('Completion time:', NEON_GREEN, self.game.font, (WIDTH * 0.5, HEIGHT * 0.55))
+		self.game.render_text(str(self.time_elapsed), NEON_GREEN, self.game.font, (WIDTH * 0.5, HEIGHT * 0.6))
--- sprites.py
+++ sprites.py
@@ -1,7 +1,8 @@
 import pygame, math
 from settings import *
 from message import Message
-from intermission import Intermission
+#from intermission import Intermission
+from demo_end import Intermission
 
 class FadeSurf(pygame.sprite.Sprite):
 	def __init__(self, game, scene, groups, pos, alpha = 255, z = LAYERS['foreground']):
@@ -33,7 +34,8 @@ class FadeSurf(pygame.sprite.Sprite):
 				self.alpha = 255
 				self.scene.exit_state()
 				if SCENE_DATA[self.scene.current_scene]['unit'] != SCENE_DATA[self.scene.new_scene]['unit']:
-					Intermission(self.game, self.scene, self.scene.new_scene).enter_state()
+					#Intermission(self.game, self.scene, self.scene.new_scene).enter_state()
+					Intermission(self.game, self.scene).enter_state()
 				else:
 					self.scene.create_scene(self.scene.prev_level, self.scene.new_scene)
 		
